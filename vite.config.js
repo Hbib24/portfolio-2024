cc323d133a9791129e3b742ffe9e42745d53806a
Alpha <habib.bekir@gmail.com>
2023-12-31 20:24:06 +0100

    gh-pages fixed base

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import { resolve, path } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     vue({
       template: { transformAssetUrls },
