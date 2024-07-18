dccb7af5d038df94debefa23c1d6a0790483259b
hbib <hbib.bekir@gmail.com>
2024-07-18 23:27:20 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: { Navbar },
+  data: () => ({
+    darkMode: false,
+  }),
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+    },
+  },
+  created() {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      this.darkMode = true;
+    }
+  },
+};
+</script>
+
+<template>
+  <div :class="{ dark: darkMode }">
+    <Navbar :darkMode="darkMode" :toggleDarkMode="toggleDarkMode" />
+    <RouterView />
+  </div>
+</template>
