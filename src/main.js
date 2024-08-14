68d6a22ef7be363c9d7fb0796b5a94cf58242eef
Your Name <you@example.com>
2024-08-14 15:41:03 +0100

    removed router

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import "flowbite/dist/flowbite";
 import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
 import App from "./App.vue";
-import router from "./router";
 
 import fr from "./locales/fr.json";
 import en from "./locales/en.json";
@@ -24,7 +23,6 @@ const i18n = createI18n({
 
 const app = createApp(App);
 
-app.use(router);
 app.use(i18n);
 
 app.mount("#app");
